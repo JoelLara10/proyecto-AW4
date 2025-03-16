87949c7541729cf967b9ed4812b848cf39700ae3
root <root@ip-172-31-5-237.us-east-2.compute.internal>
2025-03-16 02:22:56 +0000

    prueba ec2

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 from flasgger import Swagger
 import os
 from flask_jwt_extended import JWTManager
+from flask.cli import with_appcontext
+import click
 
 # Cargar variables de entorno
 load_dotenv()
@@ -19,11 +21,6 @@ jwt = JWTManager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# app.config['SWAGGER'] = {
-#     'title': 'Mi API Flask',
-#     'uiversion': 3
-# }
-
 # Inicializar extensiones
 db.init_app(app)
 migrate.init_app(app, db)
@@ -33,6 +30,14 @@ swagger = Swagger(app)
 # Registrar rutas
 from app.routes.user import user_bp
 app.register_blueprint(user_bp, url_prefix='/users')
+
+# Comando personalizado para inicializar la base de datos
+@app.cli.command('init-db')
+@with_appcontext
+def init_db():
+    """Inicializa la base de datos creando las tablas"""
+    db.create_all()
+    click.echo('Base de datos inicializada correctamente.')
 
 if __name__ == '__main__':
     app.run(debug=True)
